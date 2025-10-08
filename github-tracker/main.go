0bb32b4aa5c14b48f2be2e519a2fc7501f9d1a0f
haleckziszhoria <amcsoria@gmail.com>
2025-10-07 20:57:32 -0500

    github actions config

--- github-tracker/main.go
+++ github-tracker/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+
+	"github-tracker/github-tracker/models"
+	"github-tracker/github-tracker/repository"
+	"github-tracker/github-tracker/repository/entity"
 )
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,14 +20,30 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	body, err := io.ReadAll(r.Body)
-
 	if err != nil {
 		fmt.Println("Error reading the request")
 
 		return
 	}
 
-	fmt.Printf(string(body))
+	fmt.Println(string(body))
+}
+
+func insertGitHubWebhook(ctx context.Context, repo repository.Commit, webhook models.GitHubWebhook, body string, createdTime time.Time) error {
+	commit := entity.Commit{
+		RepoName:       webhook.Repository.FullName,
+		CommitID:       webhook.HeadCommit.ID,
+		CommitMessage:  webhook.HeadCommit.Message,
+		AuthorUsername: webhook.HeadCommit.Author.Username,
+		AuthorEmail:    webhook.HeadCommit.Author.Email,
+		Payload:        body,
+		CreatedAt:      createdTime,
+		UpdatedAt:      createdTime,
+	}
+
+	err := repo.Insert(ctx, &commit)
+
+	return err
 }
 
 func main() {
@@ -30,11 +52,8 @@ func main() {
 	router.HandleFunc("/hello", postHandler).Methods("POST")
 
 	fmt.Println("Server listening on port 8080")
-
 	err := http.ListenAndServe(":8080", router)
-
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 }
---
+++ github-tracker/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDummy(t *testing.T) {
+	c := require.New(t)
+	result := 22
+	c.Equal(22, result)
+}
